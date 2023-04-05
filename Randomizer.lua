7b2325f1ef90629603da596e245a70d293ce2173
Constantine Of Constantinopol <107809836+AlbinAlfadorson@users.noreply.github.com>
2023-04-05 16:41:39 +0300

    Create Randomizer.lua

---
+++ Randomizer.lua
@@ -0,0 +1,9 @@
+math.randomseed(os.time())
+goodpogoda = math.random()
+komppochinili = math.random()
+
+if goodpogoda > komppochinili then
+  print("go to GYM")
+else
+  print("Lerning C++")
+  end
